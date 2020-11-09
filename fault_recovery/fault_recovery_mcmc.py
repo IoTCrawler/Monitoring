ebb17de64707c61229c78c23d7421f87b3e9ba9c
Marten Fischer <m.fischer@hs-osnabrueck.de>
2020-11-09 15:41:59 +0100

    attempt to use the number of minutes since midnight as timestamp for MCMC

--- fault_recovery/fault_recovery_mcmc.py
+++ fault_recovery/fault_recovery_mcmc.py
@@ -23,9 +23,16 @@ class FaultRecoveryMCMC:
     def makeModelFilename(self, filename):
         return os.path.join(os.path.dirname(os.path.abspath(__file__)), "models", filename.replace(':', '_'))
 
+    def minutesSinceMidnight(self, timestamp):
+        return timestamp.hour * 60 + timestamp.minute
+        # return timestamp.day * 1440 + timestamp.hour * 60 + timestamp.minute
+
     def newSensor(self, sensorID, entity):
         print("FR newSensor called")
-        muValue, sigmaValue = self.get_norm_dist(sensorID)
+        muValue, sigmaValue, genNumber = self.get_norm_dist(sensorID)
+        if not muValue:
+            # no training data
+            return
         synthetic_data_model = pm.Model()
         with synthetic_data_model:
             observationProb = pm.Normal('observation_value', mu=muValue, sigma=sigmaValue)
@@ -37,14 +44,17 @@ class FaultRecoveryMCMC:
             else:
                 step = pm.NUTS()
 
-            trace = pm.sample(self.generatedNumber, step=step, init='advi+adapt_diag', cores=1, chains=1)
+            # trace = pm.sample(self.generatedNumber, step=step, init='advi+adapt_diag', cores=1, chains=1)
+            trace = pm.sample(genNumber, step=step, init='advi+adapt_diag', cores=1, chains=1)
         fname = pm.save_trace(trace, 'LearningOutput' + str(sensorID) + '.trace', overwrite=True)
         traceArray = trace['out']
         final = []
-        for i in range(self.burninTime):
+        print("lta:", len(traceArray))
+        # for i in range(self.burninTime):
+        for i in range(genNumber):
             final.append(traceArray[i])
         finalArray = np.asarray(final)
-
+        print("N:", len(finalArray))
         pickle_out = open(self.makeModelFilename("modelout_" + sensorID + ".pickle"), "wb")
         pickle.dump(finalArray, pickle_out)
         pickle_out.close()
@@ -52,20 +62,24 @@ class FaultRecoveryMCMC:
 
     def update(self, sensorID, value):
         print("FR update callled")
-        timeStamp = dparser.parse(str(value), fuzzy=True).timestamp()
-        print(timeStamp)
+        # timeStamp = dparser.parse(str(value), fuzzy=True).timestamp() % 10000
+        timeStamp = np.float64(self.minutesSinceMidnight(dparser.parse(str(value), fuzzy=True)))
+        print("current ts:", timeStamp)
         missingValuePosition = 4
         # pickle_in_data = open("models/modelout.pickle", "rb")
         pickle_in_data = open(self.makeModelFilename("modelout_" + sensorID + ".pickle"), "rb")
         pickle_in_time = open(self.makeModelFilename("timestamps_" + sensorID + ".pickle"), "rb")
-        myinputTime = pickle.load(pickle_in_time) % 10000  # extract the time
-        print(myinputTime)
+        # myinputTime = pickle.load(pickle_in_time) % 10000  # extract the time
+        myinputTime = pickle.load(pickle_in_time)
+        print("U1:", len(myinputTime), myinputTime)
+        myinput = pickle.load(pickle_in_data)
         for i in range(len(myinputTime)):
             if (myinputTime[i] == timeStamp):
-                print("yes")
                 missingValuePosition = i
+                print("U3: found timestamp")
                 break
-        myinput = pickle.load(pickle_in_data)
+        print("U2:", missingValuePosition, myinput[abs(missingValuePosition - self.windows): abs(missingValuePosition + self.windows)], abs(missingValuePosition - self.windows), abs(missingValuePosition + self.windows), len(myinput))
+        # TODO: test if myinput is long enough
         imputedValue = int(
             np.mean(myinput[abs(missingValuePosition - self.windows): abs(missingValuePosition + self.windows)]))
         pickle_in_time.close()
@@ -84,17 +98,22 @@ class FaultRecoveryMCMC:
         for item in dataSampleItems:
             elementTime = item["timestamp"]
             timeParse = dparser.parse(str(elementTime), fuzzy=True)
-            originalValues[i][1] = timeParse.strftime("%Y%m%d%H%M")
+            # originalValues[i][1] = timeParse.strftime("%Y%m%d%H%M")
+            originalValues[i][1] = self.minutesSinceMidnight(timeParse)
             originalValues[i][0] = item["value"]
             i += 1
 
         ind = np.argsort(originalValues[:, 1])
         originalValuesSorted = originalValues[ind]
-
+        print(originalValuesSorted)
+        print("T1:", originalValuesSorted[:, 1], len(originalValuesSorted[:, 1]))
+        uniqueValues = np.unique(originalValuesSorted[:, 1])
+        print("T2:", uniqueValues, len(uniqueValues))
         timestamp_filename = self.makeModelFilename("timestamps_" + sensorID + ".pickle")
         # pickle_out = open("timestamps.pickle", "wb")
         pickle_out = open(timestamp_filename, "wb")
-        pickle.dump(originalValuesSorted[:, 1], pickle_out)
+        # pickle.dump(originalValuesSorted[:, 1], pickle_out)
+        pickle.dump(uniqueValues, pickle_out)
         pickle_out.close()
 
         originalValuesSortedFixedTimeindexes = deepcopy(originalValuesSorted)
@@ -105,14 +124,4 @@ class FaultRecoveryMCMC:
         sigmaValue = np.abs(np.max(originalValuesSortedFixedTimeindexes[:, 0])
                             - np.min(originalValuesSortedFixedTimeindexes[:, 0]))
 
-        return muValue, sigmaValue
-
-# def _newSensor(self, sensorID, entity):
-# 	print("FR newSensor called")
-#
-# 	ngsi_id, ngsi_type = ngsi_parser.get_IDandType(entity)
-# 	result = utils.loadTrainingData(ngsi_id)
-# 	print(result)
-
-# def update(self, sensorID, value):
-# 	print("FR update callled")
+        return muValue, sigmaValue, len(uniqueValues)
